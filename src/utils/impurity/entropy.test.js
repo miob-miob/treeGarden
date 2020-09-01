0f7e31862f7911a7c004f58d0cb1f7caf8ab2e4a
michal_miob <michal.miob@gmail.com>
2020-09-01 12:03:12 +0200

    Added tests ts config

--- src/utils/impurity/entropy.test.js
+++ src/utils/impurity/entropy.test.js
@@ -1,6 +1,7 @@
-// @ts-ignore
 import { simple } from '../../sampleDataSets';
-import { getInformationGainForSplitCriteria, getEntropyOfDataSet } from './entropy';
+import {
+  getInformationGainForSplitCriteria, getEntropyOfDataSet, getInformationGain, getEntropy
+} from './entropy';
 import { getClassesOfDataSet } from '../dataSet/set';
 import { getSplitCriteriaFn } from '../dataSet/split';
 
@@ -8,10 +9,22 @@ import { getSplitCriteriaFn } from '../dataSet/split';
 const knownClasses = getClassesOfDataSet(simple);
 const entropyOfSimpleDataSet = getEntropyOfDataSet(simple, knownClasses);
 
-test('getClassesOfDataSet', () => {
+test('getEntropyOfDataSet', () => {
   const manuallyCalculatedEntropy = -((2 / 5) * Math.log2(2 / 5) + (3 / 5) * Math.log2(3 / 5));
   expect(entropyOfSimpleDataSet).toBeCloseTo(manuallyCalculatedEntropy);
 });
+
+test('getInformationGain', () => {
+  const rootFrequencies = [10, 10];
+  const branchA = [1, 4];
+  const branchB = [7, 8];
+  const rootEntropy = getEntropy(rootFrequencies);
+  const branchAEntropy = getEntropy(branchA);
+  const branchBEntropy = getEntropy(branchB);
+  expect(getInformationGain(rootFrequencies, [branchA, branchB]))
+    .toBeCloseTo(rootEntropy - ((5 / 20) * branchAEntropy + (15 / 20) * branchBEntropy));
+});
+
 test('getInformationGainForSplitCriteria', () => {
   const splitCriteriaFn = getSplitCriteriaFn('color', '==');
   expect(getInformationGainForSplitCriteria(simple, splitCriteriaFn, knownClasses)).toBeCloseTo(entropyOfSimpleDataSet);
