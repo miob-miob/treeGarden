2ec75d911aeddfd4f296628c0249aecd96cacdd5
michal_miob <michal.miob@gmail.com>
2022-10-17 22:21:00 +0200

    publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
-current_version=$(node scripts/logPackageVersion.js)
+set -e
 
-echo "version from package json: ${current_version}"
+# some validation of args - first is major/minor/patch second should be github access token
+if [[ $# -lt 2 ]] ; then
+    echo 'You must provide two arguments: version_type + github token'
+    exit 1
+fi
+if [ "$1" == "major" ]
+then
+  echo "Will bump major"
+elif [ "$1" == "minor" ]
+then
+  echo "Will bump minor"
+elif [ "$1" == "patch" ]
+then
+  echo "Will bump patch"
+else
+  echo "version type must be major, minor or patch"
+  exit 1
+fi
 
+# todo remove
+git remote add ci_origin "https://miob-miob:$2@github.com/miob-miob/treeGarden"
+
+npm version "$1" -m "Publish tree-garden %s"
+
+# push generated changes into package.json + package.lock.json
+git push ci_origin
+
+# extract new version from package.json
+new_version=$(node scripts/logPackageVersion.js)
+
+# push newly created tag
+git push ci_origin v"${new_version}"
