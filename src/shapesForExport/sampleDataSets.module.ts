0f89496d1f29b9fefdfb61277b1dc2a1de218f9f
michal_miob <michal.miob@gmail.com>
2022-11-18 20:08:40 +0100

    api docs continue

--- src/shapesForExport/sampleDataSets.module.ts
+++ src/shapesForExport/sampleDataSets.module.ts
@@ -1,6 +1,9 @@
 // todo if we go hardly after bundle size - this would not be ideal
 
-
+/** generating MD this takes to much time - lets hide it for now
+ * @module
+ * @hidden
+ */
 import {
   simpleSet,
   titanicSet,
--- typedoc.js
+++ typedoc.js
@@ -8,13 +8,12 @@ module.exports = {
   entryPointStrategy:'expand',
 //  excludeNotDocumented:true,
 //  json:'treeGardenSchema.json',
-  out: './rawHtmlDocs',
+  out: './docs/pages/api',
   sort:'source-order',
-  intentionallyNotExported: [],   // can be useful
 
   // settings for markdown renderer plugin
   hideBreadcrumbs:true,
 //  hideInPageTOC:true,
-  hideMembersSymbol:true,
+  hideMembersSymbol:true
 
 }
